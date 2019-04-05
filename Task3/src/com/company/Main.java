9838bd4bb8e3e177ce72d5cf851e92857efb4b81
Nataly <npoltorak@gmail.com>
2019-04-05 20:22:47 +0300

    Task3

---
+++ Task3/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+int n1 = 1;
+int n2 = 2;
+int n3;
+int a;
+int b =40;
+        System.out.print(n1+" "+n2);
+for(a=2;a<b;++a) {
+            n3=n1+n2;
+    System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
+        }
+    }
+}
